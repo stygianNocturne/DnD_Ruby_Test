d3e75f531d4738f5cfe7d58fb1d4f019e2c528f1
Ryan Stewart <stewart.rya@husky.neu.edu>
2017-01-15 21:07:11 -0500

    Create homepage.  Add devise and cancancan for user authentication and authorization

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,6 @@
+# Controller for the home page of this site
+class MainController < ApplicationController
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -62,5 +63,7 @@ Rails.application.routes.draw do
   get 'character/edit'
   get 'character/delete'
   get 'character/update'
+
+  root 'main#index'
 
 end
